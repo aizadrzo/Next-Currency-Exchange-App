b5f507db0eb2967b7472ca8d9a3958e5a3aca46a
aizadrzo <aizadridzoo@gmail.com>
2025-03-20 07:27:21 +0800

    chore: update ui, implement loading skeleton and empty search results

--- src/components/CurrencyTable.tsx
+++ src/components/CurrencyTable.tsx
@@ -12,6 +12,7 @@ import {
 } from "./ui/table";
 import { Input } from "./ui/input";
 import { Currency } from "@/app/types";
+import { Search } from "lucide-react";
 
 export function CurrencyTable({ data }: { data: Currency[] }) {
   const [search, setSearch] = React.useState("");
@@ -55,54 +56,76 @@ export function CurrencyTable({ data }: { data: Currency[] }) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {filteredData?.map((curr) => (
-            <TableRow key={curr.currency}>
-              <TableCell className="flex items-center gap-2">
-                <div className="relative w-10">
-                  <Image
-                    className="relative z-10"
-                    src={imageUrl(Currencies[curr.currency].code.toLowerCase())}
-                    width={24}
-                    height={24}
-                    alt={curr.currency}
-                  />
-                  <Image
-                    className="absolute top-0 left-[35%]"
-                    src={imageUrl(
-                      Currencies[curr.baseCurrency].code.toLowerCase()
-                    )}
-                    width={24}
-                    height={24}
-                    alt={curr.currency}
-                  />
+          {filteredData.length < 1 ? (
+            <TableRow className="text-center hover:bg-inherit">
+              <TableCell colSpan={4} className="py-10 sm:py-12">
+                <div className="flex flex-col items-center justify-center space-y-4">
+                  <div className="p-2 bg-muted/50 rounded-full w-fit">
+                    <Search className="w-8 h-8" />
+                  </div>
+                  <h3 className="text-xl sm:text-2xl font-bold">
+                    No result for "{search}"
+                  </h3>
+                  <p className="mt-1 text-sm sm:text-base max-w-md w-fit">
+                    We couldn’t find a currency with that name.
+                  </p>
                 </div>
-                <div>
-                  <span className="font-bold">
-                    {curr.currency}/{curr.baseCurrency}
-                  </span>{" "}
-                  <span className="text-neutral-500 hidden sm:block">
-                    {Currencies[curr.currency].name} /{" "}
-                    {Currencies[curr.baseCurrency].name}
-                  </span>
-                </div>
-              </TableCell>
-              <TableCell className="text-right">{curr.formatRates}</TableCell>
-              {parseFloat(curr.changePercentage) < 0 ? (
-                <TableCell className="text-right">
-                  <span className="text-red-600">{curr.changePercentage}%</span>
-                </TableCell>
-              ) : (
-                <TableCell className="text-right">
-                  <span className="text-green-600">
-                    {curr.changePercentage}%
-                  </span>
-                </TableCell>
-              )}
-              <TableCell className="text-right">
-                {parseFloat(curr.change.toString()).toFixed(4)}
               </TableCell>
             </TableRow>
-          ))}
+          ) : (
+            filteredData?.map((curr) => (
+              <TableRow key={curr.currency}>
+                <TableCell className="flex items-center gap-2">
+                  <div className="relative w-10">
+                    <Image
+                      className="relative z-10"
+                      src={imageUrl(
+                        Currencies[curr.currency].code.toLowerCase()
+                      )}
+                      width={24}
+                      height={24}
+                      alt={curr.currency}
+                    />
+                    <Image
+                      className="absolute top-0 left-[35%]"
+                      src={imageUrl(
+                        Currencies[curr.baseCurrency].code.toLowerCase()
+                      )}
+                      width={24}
+                      height={24}
+                      alt={curr.currency}
+                    />
+                  </div>
+                  <div>
+                    <span className="font-bold">
+                      {curr.currency}/{curr.baseCurrency}
+                    </span>{" "}
+                    <span className="text-neutral-500 hidden sm:block">
+                      {Currencies[curr.currency].name} /{" "}
+                      {Currencies[curr.baseCurrency].name}
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell className="text-right">{curr.formatRates}</TableCell>
+                {parseFloat(curr.changePercentage) < 0 ? (
+                  <TableCell className="text-right">
+                    <span className="text-red-600">
+                      {curr.changePercentage}%
+                    </span>
+                  </TableCell>
+                ) : (
+                  <TableCell className="text-right">
+                    <span className="text-green-600">
+                      {curr.changePercentage}%
+                    </span>
+                  </TableCell>
+                )}
+                <TableCell className="text-right">
+                  {parseFloat(curr.change.toString()).toFixed(4)}
+                </TableCell>
+              </TableRow>
+            ))
+          )}
         </TableBody>
       </Table>
     </>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+export function Hero() {
+  return (
+    <div className="mx-auto px-4 py-16 sm:py-32 lg:flex lg:items-center">
+      <div className="mx-auto max-w-xl text-center">
+        <h1 className="text-3xl font-extrabold sm:text-5xl">Latest Rates</h1>
+        <p className="mt-4 sm:text-xl/relaxed">
+          Get real-time currency rates for travel, transactions, or everyday
+          use—all in one place.
+        </p>
+      </div>
+    </div>
+  );
+}
